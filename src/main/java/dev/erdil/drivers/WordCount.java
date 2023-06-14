af95f1492ecc0f0733cff7cac9bfd94876a8b8e7
Omer Erdil Albayrak <omer-erdil.albayrak@klarna.com>
2023-06-14 09:45:40 +0200

    Add filter, tokenization, and group by to word count exercise

--- src/main/java/dev/erdil/drivers/WordCount.java
+++ src/main/java/dev/erdil/drivers/WordCount.java
@@ -1,30 +1,33 @@
 package dev.erdil.drivers;
 
-import dev.erdil.helpers.DataSetHelpers;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.MapOperator;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 
-import static dev.erdil.helpers.DataSetHelpers.printDatasetInfo;
+import java.util.List;
 
 public class WordCount {
     public static void main(String[] args) throws Exception {
         final ExecutionEnvironment env = ExecutionEnvironment.createLocalEnvironment();
-
         final ParameterTool params = ParameterTool.fromArgs(args);
-
         env.getConfig().setGlobalJobParameters(params);
 
         DataSet<String> texts = env.readTextFile("resources/wc.txt");
 
-        //env.execute("WordCount Example");
-
-        //printDatasetInfo(texts, 10);
-
         DataSet<String> filteredTexts = texts.filter(value -> value.startsWith("N"));
 
-        System.out.println(env.getConfig());
+        MapOperator<String, Tuple2<String, Integer>> tokenizedTexts = filteredTexts.map(new MapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2<String, Integer> map(String value) throws Exception {
+                return new Tuple2<String, Integer>(value, 1);
+            }
+        });
 
-        //printDatasetInfo(filteredTexts, 10);
+        List<Tuple2<String, Integer>> result = tokenizedTexts.groupBy(0).sum(1).collect();
+
+        //env.execute("WordCount Example");
     }
 }
